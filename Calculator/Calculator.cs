ce2465760f7ea21fa7a1dba913ddd128da9df3c5
Patrick <patrickhass84@gmail.com>
2024-02-13 11:09:53 +0100

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorCalc
+    {
+        public CalculatorCalc() { }
+        public int Add(int x, int y) 
+            { return x + y; }
+        public int Subtract(int x, int y) 
+            {  return x - y; }
+        public double Divide(double x, double y) 
+            { return x / y; }
+        public int Multiply(int x, int y) 
+            { return x * y; }
+    }   
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 0;
+            int x = 0;
+            string calculation;
+            double result = 0;
+            char mathing = ' ';
+
+            CalculatorCalc calc = new CalculatorCalc();
+
+
+            Console.Write("Indtast din første værdi: ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("Indtast din anden værdi: ");
+             y = int.Parse(Console.ReadLine());
+
+            Console.Write("Hvilken operatør vil du benytte? ");
+            mathing = char.Parse(Console.ReadLine());   
+
+            switch (mathing)
+            {
+                case '+':
+                    result = calc.Add(x, y);
+                    break;
+                case '-':
+                    result = calc.Subtract(x, y);
+                    break;
+                case '*':
+                    result = calc.Multiply(x, y);
+                    break;
+                case '/':
+                    result = calc.Divide(x, y);
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine("Resultatet er: " + result);
+            double divide = calc.Divide(8, 3);
+            Console.WriteLine(divide);
+        }
+    }
+}
